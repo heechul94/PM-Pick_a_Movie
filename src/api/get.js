373aa4eb1486c357a21fc4d351f5f367d03caccc
heechul94 <yhcu3517@gmail.com>
2024-01-02 01:23:36 +0900

    :truck: move: get.js file move -> src -> api

---
+++ src/api/get.js
@@ -0,0 +1,21 @@
+const options = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiY2UwNTBkOWMyN2MxMDdmYTEyNWZjZThlYTBjZmJiNiIsInN1YiI6IjY1OGQ5ZTYxMjBlNmE1NjA3ZjhiNDg3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KmZHgzG37o9llAKngwCHFkA8b_zYN5SCCnUcsAQtT5o",
+    },
+};
+
+export default async function getMovieList () {
+    try {
+        const result = await fetch(
+            "https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1",
+            options
+        );
+        const data = await result.json();
+        return data;
+    } catch (error) {
+        alert(error)
+    }
+};
