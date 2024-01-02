fd49ed9b787c6187f8c264e460b1cf593f9667da
heechul94 <yhcu3517@gmail.com>
2024-01-02 16:19:42 +0900

    :sparkled: feat: add movieCard click event

--- src/common/main.js
+++ src/common/main.js
@@ -1,8 +1,10 @@
-import getMovieList from "../api/get.js"
-import newCard from "./Card.js"
+import getMovieList from "../api/get.js";
+import newCard from "./card.js";
+import clickEvent from "./clickId.js";
 
 const data = await getMovieList();
-let dataArr = data.results;
+let dataArr = [];
+dataArr = data.results;
 dataArr.forEach( e =>{
     const card = newCard();
     const list = document.querySelector('.movieList');
@@ -11,5 +13,6 @@ dataArr.forEach( e =>{
     card.querySelector('.title').innerText = e.title;
     card.querySelector('.vote_average').innerText = e.vote_average;
     card.querySelector('.overview').innerText = e.overview;
+    clickEvent(card);
     list.appendChild(card);
-})
+});
