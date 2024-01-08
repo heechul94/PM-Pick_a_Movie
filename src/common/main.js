ebebd114349f6801a6a78fb6eec99e6998dafdce
heechul94 <yhcu3517@gmail.com>
2024-01-09 02:42:31 +0900

    :recycle: refactor: 카드생성 함수화, api response 구조분해할당

--- src/common/main.js
+++ src/common/main.js
@@ -1,26 +1,13 @@
 import {getMovieList} from "../api/get.js";
-import newCard from "./card.js";
-import {clickId, clickSubmitButton,} from "./click.js";
+import { showCards } from "./card.js";
+import { clickSubmitButton } from "./click.js";
 
 const $searchButton = document.querySelector('.searchButton');
 document.addEventListener("DOMContentLoaded",async ()=>{
-    const data = await getMovieList();
+    const { results } = await getMovieList();
     let dataArr = [];
-    dataArr = data.results;
-
-    dataArr.forEach( e =>{
-        const card = newCard();
-        const list = document.querySelector('.movieList');
-        card.id = e.id;
-        e.poster_path
-            ? card.querySelector('.poster').src = `https://image.tmdb.org/t/p/original/${e.poster_path}`
-            : card.querySelector('.poster').src = `../../images/noImage.jpg`
-        card.querySelector('.title').innerText = e.title;
-        card.querySelector('.vote_average').innerText = `평점 : ${e.vote_average}`;
-        card.querySelector('.overview').innerText = e.overview;
-        clickId(card);
-        list.appendChild(card);
-    });
+    dataArr = results;
+    showCards(dataArr);
     clickSubmitButton($searchButton,dataArr);
 })
 
