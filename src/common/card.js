2e619acf061e79c4b900c0e844eab7d71a008d14
heechul94 <yhcu3517@gmail.com>
2024-01-10 12:52:46 +0900

    update: prettier 세팅, 코드 포맷 변경

--- src/common/card.js
+++ src/common/card.js
@@ -1,36 +1,35 @@
 import { clickId } from "./click.js";
 
-export function newCard (){
+export function newCard() {
+  const $newCard = document.createElement("li");
+  const $newPoster = document.createElement("img");
+  const $newTitle = document.createElement("h2");
+  const $newRate = document.createElement("span");
+  const $newContent = document.createElement("p");
 
-    const $newCard = document.createElement("li");
-    const $newPoster = document.createElement("img");
-    const $newTitle = document.createElement("h2");
-    const $newRate = document.createElement("span");
-    const $newContent = document.createElement("p");
+  $newCard.classList.add("movieCard");
+  $newPoster.classList.add("poster");
+  $newTitle.classList.add("title");
+  $newRate.classList.add("vote_average");
+  $newContent.classList.add("overview");
+  $newCard.append($newPoster, $newTitle, $newRate, $newContent);
 
-    $newCard.classList.add('movieCard');
-    $newPoster.classList.add('poster');
-    $newTitle.classList.add('title');
-    $newRate.classList.add('vote_average');
-    $newContent.classList.add('overview');
-    $newCard.append($newPoster, $newTitle, $newRate, $newContent);
-
-    return $newCard;
+  return $newCard;
 }
 
-export function showCards (data){
-    data.forEach( e => {
-        const card = newCard();
-        const list = document.querySelector('.movieList');
-        card.id = e.id;
-        e.poster_path
-        ? card.querySelector('.poster').src = `https://image.tmdb.org/t/p/w500/${e.poster_path}`
-        : card.querySelector('.poster').src = `../../images/noImage.jpg`
-        card.querySelector('.title').innerText = e.title;
-        card.querySelector('.vote_average').innerText = `평점 : ${e.vote_average}`;
-        card.querySelector('.overview').innerText = e.overview;
-        clickId(card);
-        list.appendChild(card);
-        return card;
-    });
+export function showCards(data) {
+  data.forEach((e) => {
+    const card = newCard();
+    const list = document.querySelector(".movieList");
+    card.id = e.id;
+    e.poster_path
+      ? (card.querySelector(".poster").src = `https://image.tmdb.org/t/p/w500/${e.poster_path}`)
+      : (card.querySelector(".poster").src = `../../images/noImage.jpg`);
+    card.querySelector(".title").innerText = e.title;
+    card.querySelector(".vote_average").innerText = `평점 : ${e.vote_average}`;
+    card.querySelector(".overview").innerText = e.overview;
+    clickId(card);
+    list.appendChild(card);
+    return card;
+  });
 }
--- src/common/click.js
+++ src/common/click.js
@@ -1,38 +1,33 @@
 import { showCards } from "./card.js";
 
-export function clickId (target) {
-    target.addEventListener('click', (event)=>
-        alert(`이 영화의 ID는 ${event.currentTarget.id} 입니다`)
-    )
-};
+export function clickId(target) {
+  target.addEventListener("click", (event) => alert(`이 영화의 ID는 ${event.currentTarget.id} 입니다`));
+}
 
-export async function clickSubmitButton (target,data){
-    target.addEventListener('click', async (event)=>{
-        event.preventDefault();
-        const $searchInput = document.querySelector('.searchInput');
-        const list = document.querySelector('.movieList');
-        let searchWord = $searchInput.value;
-        if(!searchWord) {
-            $searchInput.value = null;
-            $searchInput.focus();
-            return alert('검색어를 입력해주세요');
-        }
-        else{
-            const searchData = await data.filter(e=>e.title.toLowerCase().includes(searchWord.toLowerCase()))
-            if(!searchData.length){
-                $searchInput.value = null;
-                $searchInput.focus()
-                return alert('해당하는 영화가 없습니다.');
-            };
+export async function clickSubmitButton(target, data) {
+  target.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const $searchInput = document.querySelector(".searchInput");
+    const list = document.querySelector(".movieList");
+    let searchWord = $searchInput.value;
+    if (!searchWord) {
+      $searchInput.value = null;
+      $searchInput.focus();
+      return alert("검색어를 입력해주세요");
+    } else {
+      const searchData = await data.filter((e) => e.title.toLowerCase().includes(searchWord.toLowerCase()));
+      if (!searchData.length) {
+        $searchInput.value = null;
+        $searchInput.focus();
+        return alert("해당하는 영화가 없습니다.");
+      }
 
-            list.replaceChildren();
+      list.replaceChildren();
 
-            showCards(searchData);
+      showCards(searchData);
 
-            $searchInput.value = null;
-            $searchInput.focus();
-
-
-        };
-    });
-};
+      $searchInput.value = null;
+      $searchInput.focus();
+    }
+  });
+}
