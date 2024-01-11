77e9f600c1a1d27ba5300f290f71154a5cbb0f19
ocean <twyeo9006@gmail.com>
2024-01-11 17:30:51 +0900

    update:html,js 세번째수정

--- src/review.js
+++ src/review.js
@@ -1,9 +1,41 @@
 function submitReview() {
   let reviewer = document.querySelector(".reviewer").value;
   let reviewText = document.querySelector(".reviewText").value;
-  let reviewList = document.querySelector(".reviewList");
 
-  reviewList.innerHTML += "작성자: &ensp;" + reviewer + "<p>" + "리뷰:   &ensp;" + reviewText;
+  let newReview = { reviewer: reviewer, text: reviewText };
+
+  let reviews = JSON.parse(localStorage.getItem("reviews")) || [];
+
+  if (!reviewer || !reviewText) {
+    alert("작성자와 리뷰를 모두 입력하세요!");
+    return;
+  }
+
+  reviews.push(newReview);
+
+  localStorage.setItem("reviews", JSON.stringify(reviews));
+
+  displayReviews();
 }
 
-function removeReview() {}
+function removeReview() {
+  localStorage.removeItem("reviews");
+
+  displayReviews();
+}
+
+function displayReviews() {
+  let reviews = JSON.parse(localStorage.getItem("reviews")) || [];
+  let reviewList = document.querySelector(".reviewList");
+
+  reviewList.innerHTML = "";
+  if (reviews.length > 0) {
+    reviews.forEach(function (review) {
+      reviewList.innerHTML += "작성자: &ensp;" + review.reviewer + "<p>" + "리뷰:   &ensp;" + review.text + "<br><br>";
+    });
+  }
+}
+
+window.onload = function () {
+  displayReviews();
+};
