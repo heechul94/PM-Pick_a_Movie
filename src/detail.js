3ea81a8e6326a4984d9279254d032957cf1c8b2c
heechul94 <yhcu3517@gmail.com>
2024-01-16 01:05:58 +0900

    feat: 국가 영어이름 한국어로 번역 기능 생성

--- src/detail.js
+++ src/detail.js
@@ -1,9 +1,16 @@
-import { getMovieDetail } from "./api/get.js";
+import { getCountries, getMovieDetail } from "./api/get.js";
 
-let data = await getMovieDetail();
+const data = await getMovieDetail();
+const countryData = await getCountries();
 const { genres, production_countries } = data;
 const genre = genres.map((e) => e.name);
 const countries = production_countries.map((e) => e.name);
+const nativeName = countries.map((item) => {
+  return countryData.find((e) => {
+    return e.english_name === item;
+  }).native_name;
+});
+console.log(nativeName);
 
 document.querySelector(".backdrop-image").src = `https://image.tmdb.org/t/p/original/${data.backdrop_path}`;
 document.querySelector(".movie-title").innerText = data.title;
@@ -11,5 +18,5 @@ document.querySelector(".movie-original-title").innerText = data.original_title;
 document.querySelector(".overview").innerText = data.overview;
 document.querySelector(".release").innerText = data.release_date;
 document.querySelector(".genres").innerText = genre;
-document.querySelector(".countries").innerText = countries;
+document.querySelector(".countries").innerText = nativeName;
 document.querySelector(".runtime").innerText = `${parseInt(data.runtime / 60)}시간 ${data.runtime % 60}분`;
